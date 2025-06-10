8f526446a5f31ea2b6e7d1bddc8e5d59eb2f21d9
Gokula-Nandhan <gokula.nandhan02@gmail.com>
2025-06-10 19:03:47 +0530

    added already created files from tutorial-04

---
+++ reverse_counter_20233027.py
@@ -0,0 +1,14 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:\Users\User\Documents\tutorial 04\loop.py =============
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
