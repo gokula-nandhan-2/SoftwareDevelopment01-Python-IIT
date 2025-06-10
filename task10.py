d302f82b685a4927ff745f272017beafd3243611
Gokula Nandhan <gokula.20233027@iit.ac.lk>
2025-06-10 12:33:19 +0530

    created task 10 - 13

---
+++ task10.py
@@ -0,0 +1,4 @@
+temp = float(input("Enter temperature in celcius:" ))
+
+print("Temperature in Fahrenheit is: ", (temp * 9/5) + 32)
+print("Temperature in Kelvin is: ", temp + 273.15)
---
+++ task11.py
@@ -0,0 +1,6 @@
+item1 = float(input("Enter price of item-1:"))
+item2 = float(input("Enter price of item-2:"))
+item3 = float(input("Enter price of item-3:"))
+
+total = item1 + item2 + item3
+print("Total price of items is: ", total)
