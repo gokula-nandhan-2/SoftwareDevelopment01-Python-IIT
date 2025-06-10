f6af36aadad1de644764dc2cdc75598eaeb00710
Gokula-Nandhan <gokula.nandhan02@gmail.com>
2025-06-10 11:25:05 +0530

    tutorial 1 commits

---
+++ basic_calculator_20233027.py
@@ -0,0 +1,14 @@
+a=int(input("Enter num 1 :"))
+b=int(input("Enter num 2 :"))
+
+c=a+b
+print("Addition of",a,"and",b,"is :",c)
+
+d=a-b
+print("Substraction of",a,"and",b,"is :",d)
+
+e=a*b
+print("Multiplication of",a,"and",b,"is :",e)
+
+f=a/b
+print("Division of",a,"and",b,"is :",f)
